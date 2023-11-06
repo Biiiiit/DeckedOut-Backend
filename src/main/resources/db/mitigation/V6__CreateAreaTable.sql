9e5979fc95f95c872aa69ff55af688d07d385624
Biiiiit <rubenbekema@gmail.com>
2023-11-06 09:44:33 +0100

    added database table to Area

---
+++ src/main/resources/db/mitigation/V6__CreateAreaTable.sql
@@ -0,0 +1,14 @@
+CREATE TABLE areas(
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(MAX),
+    description varchar(MAX)
+    backgroundSprite BLOB,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE area_levels (
+    area_id int,
+    level_id int,
+    FOREIGN KEY (area_id) REFERENCES areas(id),
+    FOREIGN KEY (level_id) REFERENCES levels(id)
+);
