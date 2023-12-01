71dd6f5886e202446ba2f1228053ca9847f49cf9
Biiiiit <rubenbekema@gmail.com>
2023-12-01 15:24:42 +0100

    @RolesAllowed commented out since its not working

--- src/main/java/strategy_card_game/Controller/UserController.java
+++ src/main/java/strategy_card_game/Controller/UserController.java
@@ -1,14 +1,13 @@
 package strategy_card_game.Controller;
 
-import jakarta.annotation.security.RolesAllowed;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import strategy_card_game.Business.User.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import strategy_card_game.Domain.User.*;
 
 import java.util.Optional;
@@ -26,7 +25,7 @@ public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @GetMapping("{identifier}")
-    @RolesAllowed({"normal", "admin"})
+    //@RolesAllowed({"normal", "admin"})
     public ResponseEntity<User> getUserByIdOrUsername(@PathVariable(value = "identifier") String identifier) {
         try {
             long userId = Long.parseLong(identifier);
